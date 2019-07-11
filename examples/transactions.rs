a1d8dbbb593bfc68e104e2dd4f68ce6101389ca4
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-07-11 11:06:01 +0200

    Add time and multiple expenses in example

--- examples/transactions.rs
+++ examples/transactions.rs
@@ -1,11 +1,26 @@
+use chrono::prelude::*;
+
 extern crate plusminus;
 use plusminus::{Expense, MemoryStore, Store};
 
 fn main() {
-    let simple_expense = Expense { amount: 1250 };
-
     let mut store = MemoryStore::empty();
-    let id = store.save(simple_expense);
 
-    assert_eq!(Some(&simple_expense), store.read(&id));
+    let expense1 = Expense {
+        amount: 1250,
+        time: Utc::now(),
+    };
+    let id1 = store.save(expense1);
+    assert_eq!(Some(&expense1), store.read(&id1));
+
+    let expense2 = Expense {
+        amount: 2000,
+        time: Utc::now(),
+    };
+    let id2 = store.save(expense2);
+    assert_eq!(Some(&expense2), store.read(&id2));
+
+    for (actual, expected) in store.all().iter().zip(vec![expense2, expense1].iter()) {
+        assert_eq!(&expected, actual);
+    }
 }
